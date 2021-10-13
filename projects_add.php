28d676b53c0c1b699193742b2f7ae20454a03539
Adam Thomas <thomasadam83@hotmail.com>
2021-10-13 14:20:12 -0400

    Added project add page

--- projects_add.php
+++ projects_add.php
@@ -14,43 +14,33 @@ if( isset( $_POST['title'] ) )
   if( $_POST['title'] and $_POST['content'] )
   {
     
-    $query = 'INSERT INTO articles (
+    $query = 'INSERT INTO projects (
         title,
-        instagramId,
-        twitterId,
-        soundcloudId,
-        resources,
         content,
         date,
         type,
-        home,
         url
       ) VALUES (
          "'.mysqli_real_escape_string( $connect, $_POST['title'] ).'",
-         "'.mysqli_real_escape_string( $connect, $_POST['instagramId'] ).'",
-         "'.mysqli_real_escape_string( $connect, $_POST['twitterId'] ).'",
-         "'.mysqli_real_escape_string( $connect, $_POST['soundcloudId'] ).'",
-         "'.mysqli_real_escape_string( $connect, $_POST['resources'] ).'",
          "'.mysqli_real_escape_string( $connect, $_POST['content'] ).'",
          "'.mysqli_real_escape_string( $connect, $_POST['date'] ).'",
          "'.mysqli_real_escape_string( $connect, $_POST['type'] ).'",
-         "'.mysqli_real_escape_string( $connect, $_POST['home'] ).'",
          "'.mysqli_real_escape_string( $connect, $_POST['url'] ).'"
       )';
     mysqli_query( $connect, $query );
     
-    set_message( 'Article has been added' );
+    set_message( 'Project has been added' );
     
   }
   
-  header( 'Location: articles.php' );
+  header( 'Location: projects.php' );
   die();
   
 }
 
 ?>
 
-<h2>Add Article</h2>
+<h2>Add Project</h2>
 
 <form method="post">
   
@@ -77,21 +67,6 @@ if( isset( $_POST['title'] ) )
   
   <br>
   
-  <label for="instagramId">Instagram ID:</label>
-  <input type="text" name="instagramId" id="instagramId">
-    
-  <br>
-  
-  <label for="twitterId">Twitter ID:</label>
-  <input type="text" name="twitterId" id="twitterId">
-  
-  <br>
-  
-  <label for="soundcloudId">SoundCloud ID:</label>
-  <input type="text" name="soundcloudId" id="soundcloudId">
-  
-  <br>
-  
   <label for="url">URL:</label>
   <input type="text" name="url" id="url">
   
@@ -102,15 +77,10 @@ if( isset( $_POST['title'] ) )
   
   <br>
   
-  <label for="resources">Resources:</label>
-  <textarea name="resources" id="resources" rows="5"></textarea>
-  
-  <br>
-  
   <label for="type">Type:</label>
   <?php
   
-  $values = array( 'Industry', 'Professional', 'Research', 'Speaking', 'Technology', 'Tinkering' );
+  $values = array( 'Website', 'Graphic Design' );
   
   echo '<select name="type" id="type">';
   foreach( $values as $key => $value )
@@ -124,28 +94,11 @@ if( isset( $_POST['title'] ) )
   
   <br>
   
-  <label for="home">Home:</label>
-  <?php
-  
-  $values = array( 'Yes', 'No' );
-  
-  echo '<select name="home" id="home">';
-  foreach( $values as $key => $value )
-  {
-    echo '<option value="'.$value.'"';
-    echo '>'.$value.'</option>';
-  }
-  echo '</select>';
-  
-  ?>
-  
-  <br>
-  
-  <input type="submit" value="Add Article">
+  <input type="submit" value="Add Project">
   
 </form>
 
-<p><a href="articles.php"><i class="fas fa-arrow-circle-left"></i> Return to Article List</a></p>
+<p><a href="projects.php"><i class="fas fa-arrow-circle-left"></i> Return to Project List</a></p>
 
 
 <?php
