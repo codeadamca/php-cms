9235f0ef4f3fec100c348bbb7c8386e3d8b94267
Adam Thomas <thomasadam83@hotmail.com>
2021-10-13 13:21:43 -0400

    Added temporary Infity database details

--- includes/database.php
+++ includes/database.php
@@ -1,5 +1,10 @@
 <?php
 
-$connect = mysqli_connect( "localhost","root","password", "database" );
+$connect = mysqli_connect( 
+    "sql213.epizy.com", // Host
+    "epiz_29666120", // Username
+    "U9x2tTEPKECh", // Password
+    "epiz_29666120_sample" // Database name
+);
 
 mysqli_set_charset( $connect, 'UTF8' );
